14e357b7117c07e35b04b6e770c9faac7e06b181
DanyBelowZero <95373111+razymovsky@users.noreply.github.com>
2025-11-12 16:19:19 +0300

    Add files via upload

---
+++ AuthContext.java
@@ -0,0 +1,14 @@
+package com.zimbra.cs.account.auth;
+
+/* loaded from: AuthContext.class */
+public class AuthContext {
+    public static final String AC_ORIGINATING_CLIENT_IP = "ocip";
+    public static final String AC_REMOTE_IP = "remoteip";
+    public static final String AC_SOAP_PORT = "soapport";
+    public static final String AC_ACCOUNT_NAME_PASSEDIN = "anp";
+    public static final String AC_USER_AGENT = "ua";
+    public static final String AC_AS_ADMIN = "asAdmin";
+    public static final String AC_AUTHED_BY_MECH = "authedByMech";
+    public static final String AC_PROTOCOL = "proto";
+    public static final String AC_DEVICE_ID = "did";
+}
